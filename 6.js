bd87201d278bae417fc471195ca88c7c730e1341
vinaytech23 <48765588+vinaytech23@users.noreply.github.com>
2019-03-22 20:55:18 +0530

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,9 @@
+var myhead = document.getElementById("myh1");
+
+myhead.onclick = function () {
+myhead.innerHTML = "I did paid to your work now."	
+myhead.style.color = 'red';
+myhead.style.background = 'yellow';
+myhead.style.width = '300px';
+
+};
