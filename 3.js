ae6a64ba3c531c66cc52ddfdf10fe8269a202079
vinaytech23 <48765588+vinaytech23@users.noreply.github.com>
2019-03-22 12:05:01 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,29 @@
+
+
+var ph1 ="We want a hi electric bike.";
+//var posit = ph1.indexOf("hi");
+if (ph1.indexOf("hi") == -1) {
+	alert("not found");	
+} else {
+alert(ph1.indexOf("hi"));
+}
+/* third program
+var ph = "    tHis is A        simple        text.";
+var vin1 = "hello "
+var vin2 = "hello"
+if (vin1 === vin2)
+ {
+ 	alert("yes equal");
+ }
+ else
+ {
+ 	alert(" not equal");
+ }
+
+alert(ph);
+alert(ph.length);
+alert(ph.toUpperCase());
+alert (ph.toLowerCase());
+alert(ph.charAt(9));
+alert(ph.trim());
+*/
