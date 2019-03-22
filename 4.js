3dab3444c1d426e3519701c4654741de57948bcc
vinaytech23 <48765588+vinaytech23@users.noreply.github.com>
2019-03-22 20:55:47 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,35 @@
+/*var mutlipleValues = [];
+mutlipleValues[0] = 50;
+mutlipleValues[1] = 100;
+mutlipleValues[2] = "hello";
+mutlipleValues[3] = "01/01/2020";
+document.write("hi");
+mutlipleValues = [50,70,"hello","01/15/888","vinay-a"];
+*/
+//alert(mutlipleValues.reverse());
+//alert(mutlipleValues.sort());
+//alert(mutlipleValues.join("---"));
+//var lastele = mutlipleValues.pop();
+//alert(mutlipleValues);
+//mutlipleValues.push(123,"kumar","appu12");
+//alert(mutlipleValues);
+
+//alert(mutlipleValues.splice(2, 1, 1));
+//alert( )
+
+/*var i=0;
+while (i < mutlipleValues.length) {
+
+//document.write(mutlipleValues[i]);
+alert("the value at "+i +"is : "+mutlipleValues[i]);
+	// statement
+	i++;
+}*/
+var arry1= [500,200,600,700,900,300,600];
+var total1 =0;
+for (var i = 0; i < arry1.length; i++) {
+ 	
+ 	total1 = arry1[i] + total1;
+	
+}
+alert(total1);
---
+++ 5.js
@@ -0,0 +1,2 @@
+var myhead = document.getElementById("myheading");
+myhead.innerHTML = "Hi we are in bank now.";
